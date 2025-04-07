1dfeb768947cca155dfd7b8eb54d3b647e59c80a
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-04-07 19:29:50 +0100

    Create themeToggle.tsx

---
+++ src/components/header/themeToggle.tsx
@@ -0,0 +1,28 @@
+import * as React from "react"
+import {useTheme} from "next-themes"
+
+import {Button} from "@/components/ui/button"
+import {FaMoon, FaSun} from "react-icons/fa6";
+
+export function ThemeToggle() {
+	const { theme, setTheme } = useTheme()
+
+	const toggleTheme = () => {
+		setTheme(theme === "light" ? "dark" : "light");
+	};
+
+	return (
+		<Button variant="outline"
+				size="icon"
+				onClick={toggleTheme}
+				className="text-primary hover:bg-accent"
+		>
+			{
+				theme === "light" ?
+					<FaMoon className="size-5" />
+					: <FaSun className="size-5" />
+			}
+			<span className="sr-only">Toggle theme</span>
+		</Button>
+	)
+}

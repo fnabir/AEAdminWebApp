3fd943b07802e0170da90dbc9e0e6bd60d803d37
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-04-08 20:37:59 +0100

    Update utils functions

---
+++ src/lib/utils.tsx
@@ -0,0 +1,49 @@
+import { clsx, type ClassValue } from "clsx"
+import { twMerge } from "tailwind-merge"
+import { toast } from "sonner"
+import {child, DatabaseReference, push, ref} from "@firebase/database";
+import {database} from "@/firebase/config";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function showToast(title?: string, description?: string, variant?: "default" | "success" | "error") {
+  switch(variant) {
+    case "success":
+      toast.success(title, {
+        description: description ? description : "",
+      })
+      break
+    case "error":
+      toast.error(title, {
+        description: description ? description : "",
+      })
+      break
+    default:
+      toast(title, {
+        description: description ? description : "",
+      });
+  }
+}
+
+export function generateDatabaseKey(databaseReference: string) : string {
+  return push(child(ref(database), databaseReference)).key!
+}
+
+export function getDatabaseReference(databaseReference: string) : DatabaseReference {
+  return child(ref(database), databaseReference);
+}
+
+export function formatCurrency(initialValue: number, precision: number=0): string {
+  let signed: boolean = true
+  if (initialValue >= 0) signed = false
+
+  const strValue = String(Math.abs(initialValue));
+  const split = strValue.split('.');
+  const formattedValue = split[0].replace(/(\d)(?=(\d{3})(\d{2})*$)/g, '$1,');
+  const cents = split.length > 1 ? String(split[1]).padEnd(precision, '0') : '0'.repeat(precision);
+
+  return `${signed ? '-' : '' } ৳ ${formattedValue.split('').join('')}
+    ${precision > 0 ? '.' + cents : ''}`;
+}

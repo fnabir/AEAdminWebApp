db91021426f70f6fe4567725f701bbce50503656
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-04-08 20:38:52 +0100

    Update Home Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,13 @@
 
 import Layout from "@/components/layout";
 import Loading from "@/components/loading";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 import { auth } from "@/firebase/config";
+import { getDatabaseReference } from "@/lib/utils";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
+import { useListKeys } from "react-firebase-hooks/database";
 
 export default function Home() {
   const [user, loading, error] = useAuthState(auth);
@@ -14,6 +17,33 @@ export default function Home() {
   const breadcrumb: {text: string, link?: string}[] = [
     { text: "Home"},
   ]
+
+  const currentYear = new Date().getFullYear()
+  const [snapshotFileLastYear, loadingFileLastYear] = useListKeys(getDatabaseReference(`files/${currentYear-1}`));
+  const [snapshotFileCurrentYear, loadingFileCurrentYear] = useListKeys(getDatabaseReference(`files/${currentYear}`));
+
+  const FilesCard = ({title, count, date, year} : {title: string, count: number, date?: string, year: string|number}) => {
+    return (
+      <div className="w-full sm:w-1/2 md:w-1/3 p-1">
+        <Card className="relative overflow-hidden">
+          <div className="absolute right-1 top-0 text-primary/30 z-0 pointer-events-none select-none text-6xl font-bold leading-none">
+            {year}
+          </div>
+          <div className="relative z-10">
+            <CardHeader>
+              <CardTitle className="text-xl">{title}</CardTitle>
+            </CardHeader>
+            <CardContent className="-mt-2">
+              <div className="text-5xl font-bold">{count}</div>
+            </CardContent>
+            <CardFooter>
+              {date ? `Last updated on ${date}` : ""}
+            </CardFooter>
+          </div>
+        </Card>
+      </div>
+    )
+  }
 
   useEffect(() => {
     if (!loading && (!user || error)) {
@@ -26,8 +56,11 @@ export default function Home() {
   if (user) {
     return (
       <Layout breadcrumb={breadcrumb}>
-        <></>
-        </Layout>
+        <div className="flex flex-wrap">
+          <FilesCard title={"Total Files"} count={snapshotFileCurrentYear? snapshotFileCurrentYear.length : 0} year={currentYear}/>
+          <FilesCard title={"Total Files"} count={snapshotFileLastYear? snapshotFileLastYear.length : 0} year={currentYear-1}/>
+        </div>
+      </Layout>
     )
   }
 }

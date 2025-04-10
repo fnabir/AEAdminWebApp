4561ae6a4975a0fd13004e3cd11caa2b6b899b45
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-04-10 22:58:00 +0100

    Add Files Page

---
+++ src/app/files/page.tsx
@@ -0,0 +1,35 @@
+"use client"
+
+import Layout from "@/components/layout";
+import {useAuth} from "@/hooks/use-auth";
+import {useRouter, useSearchParams} from "next/navigation";
+import Loading from "@/components/loading";
+import React from "react";
+
+export default function FilesPage() {
+	const {user, loading} = useAuth();
+	const router = useRouter();
+	const breadcrumb: {text: string, link?: string}[] = [
+		{ text: "Home", link: "/" },
+		{ text: "/" },
+		{ text: "Files" },
+	]
+
+	const searchParams = useSearchParams()
+	const year = searchParams.has('year') ? searchParams.get('year') : new Date().getFullYear()
+
+	if (loading) return <Loading />;
+
+	if (!loading && !user) {
+		router.push("/login");
+		return null;
+	}
+
+	return (
+		<Layout breadcrumb={breadcrumb}>
+			<div className={"flex flex-col h-full"}>
+				
+			</div>
+		</Layout>
+	)
+}

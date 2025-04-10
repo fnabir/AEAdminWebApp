f67826250906d3d1633b373eb3bf635b51954e9e
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-04-10 22:59:47 +0100

    Add and update card components

---
+++ src/components/card/card-balance.tsx
@@ -0,0 +1,33 @@
+import { formatCurrency } from "@/lib/utils";
+import Link from "next/link";
+import {Badge} from "@/components/ui/badge";
+import {Card, CardFooter, CardHeader, CardTitle} from "@/components/ui/card";
+import {BalanceInterface} from "@/lib/interfaces";
+
+export default function CardBalance(props:BalanceInterface) {
+    return (
+      <Link
+        href={`/${props.type}/${props.id}`}>
+          <Card
+            className={`flex-row w-full justify-between items-center px-6 py-2
+                        ${props.status === "cancel" ? "bg-red-900 text-white hover:bg-red-900/80" : 
+                          props.value < 0 ? "bg-yellow-900 text-white hover:bg-yellow-900/80" : 
+                          props.value === 0 ? "bg-green-900 text-white hover:bg-green-900/80" : "bg-muted hover:bg-muted/80 text-primary" }`}
+            >
+            <div className={"flex-grow items-center"}>
+              <CardHeader className={"flex-row items-center space-x-2 space-y-0 p-0"}>
+                <CardTitle className="font-semibold">{props.name}</CardTitle>
+                {
+                  (props.status === "cancel" || props.value < 0) &&
+                  <Badge>{props.status == 'cancel' ? "Cancelled" : props.value < 0 ? "Overpaid" : ""}</Badge>
+                }
+              </CardHeader>
+              { props.date && <CardFooter className="text-sm p-0">{props.date}</CardFooter>}
+            </div>
+            <div className={"flex-wrap items-center text-2xl font-medium font-mono"}>
+                {formatCurrency(props.value)}
+            </div>
+          </Card>
+        </Link>
+    )
+}
---
+++ src/components/card/card-total-balance.tsx
@@ -0,0 +1,52 @@
+import { formatCurrency } from "@/lib/utils";
+import { MdUpdate } from "react-icons/md";
+import React, {useState} from "react";
+import {Card} from "@/components/ui/card";
+
+interface Props {
+    value: number
+    date: string
+    text?: string
+    update?: boolean
+    onClick?: () => void
+		className?: string
+}
+
+const CardTotalBalance: React.FC<Props> = ({
+                                         value,
+                                         date,
+                                         text,
+                                         update = false,
+                                         onClick,
+	className,
+}) => {
+	const titleText = text ? text : "Total Balance";
+	const dateText = date == null || date == '' ? 'Last update date not found' : 'Last updated on ' + (date);
+	const [updateState, setUpdateState] = useState(update);
+	const handleOnClick = () => {
+		if (onClick !== undefined) {
+			onClick()
+			setUpdateState(false);
+		}
+	};
+
+    return (
+      <Card className={`rounded-xl shadow bg-muted flex pl-6 pr-2 pt-2 ${className}`}>
+          <div className={(updateState && update ? "" : "pr-2") + " w-full mx-auto md:flex md:items-center md:justify-between text-primary"}>
+              <div>
+                  <div className="text-xl text-center md:text-start">{titleText}</div>
+                  <p id="updatedate" className="text-sm pb-2 sm:text-center md:text-start">{dateText}</p>
+              </div>
+              <div className="items-center text-right mt-3 text-3xl font-medium font-mono sm:mt-0">
+                  {formatCurrency(value)}
+              </div>
+          </div>
+          <button className={updateState && update ? "w-10 bg-black bg-opacity-40 rounded-lg hover:bg-opacity-70 m-2 p-2" : "hidden"}
+                  onClick={handleOnClick}>
+              <MdUpdate className='w-6 h-6'/>
+          </button>
+      </Card>
+    )
+}
+
+export default CardTotalBalance;

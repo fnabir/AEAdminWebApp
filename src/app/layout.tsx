f2280bbcb7f946b76307f939aca5ce345e642ba0
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-04-10 22:57:48 +0100

    Fix the global css file path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider } from "@/components/theme-provider"
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
 import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({

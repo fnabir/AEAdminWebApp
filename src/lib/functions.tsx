d69c104ee793b0363cf5d78fa7b800f6d83a5956
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-04-10 18:43:37 +0100

    Update functions.tsx

--- src/lib/functions.tsx
+++ src/lib/functions.tsx
@@ -1,5 +1,7 @@
 import { auth } from "@/firebase/config";
 import { signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { update } from "firebase/database";
+import { getDatabaseReference, showToast } from "@/lib/utils";
 
 export async function login(email: string, password: string) {
 	return signInWithEmailAndPassword(auth, email, password);
@@ -12,4 +14,18 @@ export async function logout() {
 	} catch (error) {
 		console.error("Error logging out: ", error)
 	}
+}
+
+export async function updateAccountInfo(uid: string, data: object) {
+	await update(getDatabaseReference(`info/user/${uid}`), data).then(() => {
+		showToast("Successful", "Updated the account info successfully.", "success");
+	}).catch ((error) => {
+		showToast("Error", `Failed to update the account info record: ${error.message}`, "error");
+	})
+}
+
+export async function updateTotalBalance(type: string, value: number) {
+	await update(getDatabaseReference(`balance/total/${type}`), {
+		value: value
+	})
 }

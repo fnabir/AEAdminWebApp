a38685407b2ac2db1bfd766066af0d05de588c62
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-04-10 18:44:11 +0100

    Update admin uids

--- src/hooks/use-auth.tsx
+++ src/hooks/use-auth.tsx
@@ -5,7 +5,7 @@ import { auth } from '@/firebase/config';
 
 export const useAuth = () => {
 	const [user, loading, error] = useAuthState(auth);
-	const admin = ["wGCJbfgAZIQgM6Wh5wz4x7KZlpF2"];
+	const admin = ["wGCJbfgAZIQgM6Wh5wz4x7KZlpF2", "Q34DP3VFxwTzJqnENDetTiMUZQv1", "kclUN4PVCeW69y6X28rbzr2p4kW2"];
 	const staff = ["LzcKIs2huyaK83FEOqbkJCumezu2"];
 	const userRole = user ? admin.includes(user.uid) ? "admin" : staff.includes(user.uid) ? "staff" : "user" : "";
 	return { user, userRole, loading, error };

852dc840c188219ffbd99bf57cf8ebdf50a211c9
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-04-15 11:12:03 +0100

    Add separator component

---
+++ src/components/generic/separator.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type SeparatorProps = {
+	orientation?: "horizontal" | "vertical";
+	className?: string;
+};
+
+const Separator: React.FC<SeparatorProps> = ({ orientation = "horizontal", className }) => {
+	return (
+		<div className={`${orientation == "horizontal" ? `w-full h-0.5 bg-linear-to-r` : `self-stretch w-0.5 bg-linear-to-b`}  from-transparent to-transparent via-primary rounded-full ${className}`}/>
+	)};
+
+export default Separator;

0677c89ff2d3e706eb1da704d42fa95bab362c17
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-04-16 20:29:17 +0100

    Add TransactionInterface

--- src/lib/interfaces.tsx
+++ src/lib/interfaces.tsx
@@ -1,8 +1,19 @@
 export interface BalanceInterface {
-    type:string
-    id: string
-    name: string
-    value: number
-    date?: string
-    status?: string
+  type:string
+  id: string
+  name: string
+  value: number
+  date?: string
+  status?: string
+}
+
+export interface TransactionInterface {
+  type:string
+  uid: string
+  transactionId: string
+  title: string
+  details?: string
+  value: number
+  date: string
+  access?:string
 }

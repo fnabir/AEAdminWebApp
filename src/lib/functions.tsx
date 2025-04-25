8e140d66275be5e3f39a4696c7b241f00abc5d6d
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-04-25 17:46:04 +0100

    Add updateFileExpense function

--- src/lib/functions.tsx
+++ src/lib/functions.tsx
@@ -2,6 +2,7 @@ import { auth } from "@/firebase/config";
 import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { set, update } from "firebase/database";
 import { getDatabaseReference, showToast } from "@/lib/utils";
+import { expenseDataType } from "@/lib/types";
 
 export async function login(email: string, password: string) {
 	return signInWithEmailAndPassword(auth, email, password);
@@ -42,6 +43,20 @@ export async function updateFile(fileNo: number, fileYear: number, dataInfo: obj
 	}).catch ((error) => {
 		showToast("Error", `Failed to update the file details: ${error.message}`, "error");
 	})
+}
+
+export async function updateFileExpense(fileNo: number, fileYear: number, type: "port" | "custom" | "other" | "delivery", dataSet: expenseDataType[]) {
+	const data = dataSet.reduce((acc, item) => {
+		if (item.details != "" && item.value != 0) 
+			acc[item.id] = {details: item.details, value: item.value}
+		return acc
+	}, {} as Record<number, { details: string; value: number }>)
+	try {
+		await set(getDatabaseReference(`files/expense/${fileYear}/${fileNo}/${type}`), data)
+    showToast("Successful", `Updated the file ${type} expense successfully.`, "success")
+	} catch (error) {
+		showToast("Error", `Failed to update the file ${type} expense: ${error}`, "error");
+	}
 }
 
 export async function updateTotalBalance(type: string, value: number) {

061870b14a858d792cd2487d1bce5f15a268a080
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-04-25 17:43:57 +0100

    Update breadcrumb to make mobile friendly

--- src/components/ui/breadcrumb.tsx
+++ src/components/ui/breadcrumb.tsx
@@ -14,7 +14,7 @@ function BreadcrumbList({ className, ...props }: React.ComponentProps<"ol">) {
     <ol
       data-slot="breadcrumb-list"
       className={cn(
-        "text-muted-foreground flex flex-wrap items-center gap-1.5 font-medium break-words sm:gap-2.5",
+        "text-muted-foreground flex flex-wrap items-center gap-1 font-medium break-words sm:gap-2.5 text-sm md:text-base",
         className
       )}
       {...props}

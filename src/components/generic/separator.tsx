74b51ed6f1bb95efbd93dcd018d0e7f9ab502ae1
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-05-09 09:10:07 +0100

    Add faded option in Custom Separator

--- src/components/generic/separator.tsx
+++ src/components/generic/separator.tsx
@@ -3,11 +3,13 @@ import React from "react";
 type SeparatorProps = {
 	orientation?: "horizontal" | "vertical";
 	className?: string;
+  faded?: boolean
 };
 
-const Separator: React.FC<SeparatorProps> = ({ orientation = "horizontal", className }) => {
+const Separator: React.FC<SeparatorProps> = ({ orientation = "horizontal", className, faded = true }) => {
 	return (
-		<div className={`${orientation == "horizontal" ? `w-full h-0.5 bg-linear-to-r` : `self-stretch w-0.5 bg-linear-to-b`}  from-transparent to-transparent via-primary rounded-full ${className}`}/>
+		<div className={`${orientation == "horizontal" ? `w-full h-0.5 bg-linear-to-r` : `self-stretch w-0.5 bg-linear-to-b`}
+                    ${faded ? "from-transparent via-primary to-transparent" : "bg-primary/40"} rounded-full ${className}`}/>
 	)};
 
 export default Separator;

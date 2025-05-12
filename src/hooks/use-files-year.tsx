5074ef093a52d5911f42f5e70601e3c085a0e6ca
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-05-12 09:32:00 +0100

    Create useFilesYear hook

---
+++ src/hooks/use-files-year.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import { getCurrentYear } from "@/lib/utils";
+
+export function useFilesYear(validYears: number[]) {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  const currentYear = getCurrentYear();
+  const [year, setYear] = useState(currentYear);
+
+  useEffect(() => {
+    const selectedYear = searchParams.get('year');
+    if (selectedYear) {
+      const newYear = Number(selectedYear);
+      if (validYears.includes(newYear)) {
+        setYear(newYear);
+      } else {
+        setYear(currentYear);
+        router.replace("/files");
+      }
+    } else {
+      setYear(currentYear);
+    }
+  }, [searchParams, validYears, router, currentYear]);
+
+  const changeYear = (newYear: number) => {
+    setYear(newYear);
+    router.push(`?year=${newYear}`);
+  };
+
+  return { year, changeYear };
+}

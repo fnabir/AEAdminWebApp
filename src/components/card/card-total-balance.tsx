ae696c847ab8ecba944f65f48f9cf0e8c6d4abf3
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-05-16 14:17:34 +0100

    Update CardTotalBalance to show error

--- src/components/card/card-total-balance.tsx
+++ src/components/card/card-total-balance.tsx
@@ -4,18 +4,20 @@ import React, {useState} from "react";
 import {Card} from "@/components/ui/card";
 
 interface Props {
-    value: number
-    date: string
-    text?: string
-    update?: boolean
-    onClick?: () => void
-		className?: string
+    value: number;
+    date: string;
+    text?: string;
+    error?: string;
+    update?: boolean;
+    onClick?: () => void;
+		className?: string;
 }
 
 const CardTotalBalance: React.FC<Props> = ({
                                          value,
                                          date,
                                          text,
+                                         error,
                                          update = false,
                                          onClick,
 	className,
@@ -33,14 +35,14 @@ const CardTotalBalance: React.FC<Props> = ({
     return (
       <Card className={`flex flex-row rounded-xl shadow bg-accent px-2 md:px-6 py-1 md:py-2 items-center ${className}`}>
         <div className="grow text-start">
-          <div className="text-xl ">{titleText}</div>
-          <p id="updatedate" className="text-sm">{dateText}</p>
+          <div className="text-xl ">{`${error ? "Error loading " : ""}${titleText}`}</div>
+          <p className="text-sm">{error ? error : dateText}</p>
         </div>
 
-        <div className="items-center text-right text-lg md:text-3xl font-medium font-mono">{formatCurrency(value)}</div>
+        {!error &&<div className="items-center text-right text-lg md:text-3xl font-medium font-mono">{formatCurrency(value)}</div>}
 
         { 
-          updateState && update && <button className="border-1 border-primary rounded-lg hover:bg-primary/20 ml-2 p-2 cursor-pointer duration-150" onClick={handleOnClick}>
+          !error && updateState && update && <button className="border-1 border-primary rounded-lg hover:bg-primary/20 ml-2 p-2 cursor-pointer duration-150" onClick={handleOnClick}>
             <MdUpdate size={24}/>
           </button>
         }

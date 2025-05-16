a8dca4f1e34b129ea93ae3d7cd2a8c9b3e0e5d86
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-05-16 23:45:37 +0100

    Add paymentOptions array

--- src/lib/arrays.tsx
+++ src/lib/arrays.tsx
@@ -5,3 +5,13 @@ export const fileStatusOptions = [
 	{value: 'Delivery'},
 	{value: 'Done'}
 ];
+
+export const paymentOptions = [
+	{value: 'Cash', label: 'Cash'},
+	{value: 'Cheque', label: 'Cheque'},
+	{value: 'Account Transfer', label: 'Account Transfer'},
+	{value: 'Bank Transfer', label: 'Bank Transfer'},
+	{value: 'CellFin', label: 'CellFin (Phone)'},
+	{value: 'CellFin (Account)', label: 'CellFin (Account)'},
+	{value: 'bKash', label: 'bKash'},
+];

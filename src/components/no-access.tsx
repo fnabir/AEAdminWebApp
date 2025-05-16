fc321947777a07e392b3003b0ca0910d10ebe298
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-05-16 10:42:55 +0100

    Create no-access.tsx

---
+++ src/components/no-access.tsx
@@ -0,0 +1,15 @@
+import { MdOutlineWarningAmber } from "react-icons/md";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+export default function NoAccess() {
+	return (
+		<div className='flex flex-col space-y-3 w-full h-full items-center justify-center'>
+			<MdOutlineWarningAmber className="size-12 text-amber-500"/>
+			<div className='text-base lg:text-xl font-semibold'>Not authorized to access the page.</div>
+      <Link href="/">
+        <Button>Go to Home</Button>
+        </Link>
+		</div>
+	);
+};

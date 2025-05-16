5959be949cebd3b1afc6e847afa225a92ffb8fa4
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-05-16 14:19:15 +0100

    Update interfaces.tsx

--- src/lib/interfaces.tsx
+++ src/lib/interfaces.tsx
@@ -1,19 +1,28 @@
+export interface OptionsInterface {
+  value: string;
+  label?: string;
+}
+export interface BreadcrumbInterface {
+  label: string;
+  href?: string;
+}
+
 export interface BalanceInterface {
-  type:string
-  id: string
-  name: string
-  value: number
-  date?: string
-  status?: string
+  type:string;
+  id: string;
+  name: string;
+  value: number;
+  date?: string;
+  status?: string;
 }
 
 export interface TransactionInterface {
-  type:string
-  uid: string
-  transactionId: string
-  title: string
-  details?: string
-  value: number
-  date: string
-  access?:string
+  type:string;
+  uid: string;
+  transactionId: string;
+  title: string;
+  details?: string;
+  value: number;
+  date: string;
+  access?:string;
 }

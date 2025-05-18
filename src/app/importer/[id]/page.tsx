2ab8ba1c397ebd634e304536f37f17780074ee68
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-05-18 01:23:15 +0100

    Update Importer Transaction Pages

--- src/app/importer/[id]/page.tsx
+++ src/app/importer/[id]/page.tsx
@@ -4,9 +4,8 @@ import Layout from "@/components/layout";
 import {useAuth} from "@/hooks/use-auth";
 import {usePathname, useRouter} from "next/navigation";
 import Loading from "@/components/loading";
-import React, { useEffect, useMemo } from "react";
-import { useList, useObject } from "react-firebase-hooks/database";
-import { cn, formatCurrency, getDatabaseReference, getTotalValue, showToast } from "@/lib/utils";
+import React, { useEffect } from "react";
+import { formatCurrency, showToast } from "@/lib/utils";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
 import { Skeleton } from "@/components/ui/skeleton";
 import CardIcon from "@/components/card/card-icon";
@@ -18,8 +17,9 @@ import { BreadcrumbInterface } from "@/lib/interfaces";
 import CardSection from "@/components/card/card-section";
 import { FaRegMoneyBillAlt } from "react-icons/fa";
 import { FaListUl } from "react-icons/fa6";
-import AddPaymentDialog from "./add-payment-dialog";
-import DeleteTransactionDialog from "./delete-transaction-dialog";
+import AddPaymentDialog from "@/components/transaction/add-payment-dialog";
+import { TransactionRow } from "@/components/transaction/transaction-row";
+import { useTransactionData } from "@/hooks/use-transaction-data";
 
 export default function ImporterTransactionPage() {
 	const {user, userLoading, isAdmin} = useAuth()
@@ -28,27 +28,18 @@ export default function ImporterTransactionPage() {
 	const importer: string = decodeURIComponent(path.substring(path.lastIndexOf("/") + 1));
   const router = useRouter()
 
-  const [billData, billLoading, billError] = useList(
-    user && isAdmin ? getDatabaseReference(`transaction/importer/${importer}/bill`): null
-    );
-  const [paymentData, paymentLoading, paymentError] = useList(
-    user && isAdmin ? getDatabaseReference(`transaction/importer/${importer}/payment`): null
-    );
-    const [totalBalanceSnapshot, totalBalanceLoading, totalBalanceError] = useObject(
-      user && isAdmin ? getDatabaseReference(`balance/importer/${importer}`) : null
-    );
-    const totalBalanceData = totalBalanceSnapshot?.val();
-    
-    const totalBill = useMemo(() => {
-      return getTotalValue(billData);
-    }, [billData])
-    const totalPayment = useMemo(() => {
-      return getTotalValue(paymentData);
-    }, [paymentData])
-    const totalBalance = useMemo(() => {
-        return totalBill - totalPayment;
-      }, [totalBill, totalPayment]);
-    const totalBalanceValue = totalBalanceData?.value ?? 0;
+  const {
+      dataLoading,
+      dataError,
+      billData,
+      paymentData,
+      totalBalanceData,
+      totalBill,
+      totalPayment,
+      totalBalance,
+      totalBalanceValue,
+      totalBalanceError
+    } = useTransactionData(user, isAdmin, "importer", importer);
   
   const breadcrumb: BreadcrumbInterface[] = [
     { label: "Home", href: "/" },
@@ -78,24 +69,24 @@ export default function ImporterTransactionPage() {
 		<Layout breadcrumb={breadcrumb}>
 			<div className={"flex flex-col h-full space-y-2"}>
         {
-          !billLoading && !paymentLoading && !totalBalanceLoading && totalBalance != totalBalanceValue &&
+          !dataLoading && totalBalance != totalBalanceValue &&
           <Button className="w-fit" onClick={handleUpdateTotalBalance}>Update Total Balance</Button>
         }
         <div className="grow">
           {
-            billLoading || paymentLoading ? 
+            dataLoading ? 
               <div>
                 {Array.from({ length: 4 }).map((_, i) => (
                 <Skeleton key={i} className="h-10 w-full rounded-lg my-1" />
                 ))}
               </div>
-            : billError || paymentError ? 
+            : dataError ? 
               <CardIcon
                 title={"Error"}
-                description={billError ? billError.message : paymentError?.message}>
+                description={dataError.message ?? "Error occurred retrieving transaction data."}>
                 <MdError size={28}/>
               </CardIcon>
-            : (!billData) && (!paymentData) ? 
+            : (!billData && !paymentData) ? 
               <CardIcon
                 title={"No Record Found"}>
                 <MdError size={28}/>
@@ -113,7 +104,7 @@ export default function ImporterTransactionPage() {
                     {
                       !billData || billData.length == 0 ?
                         <CardIcon
-                          title={"No Bill Record Found"}>
+                          title={"No Expense Record Found"}>
                           <MdError size={28}/>
                         </CardIcon>
                       : billData.map((item) => {
@@ -175,7 +166,7 @@ export default function ImporterTransactionPage() {
                     <div>Total Payment</div>
                     <div>{formatCurrency(totalPayment, 2)}</div>
                   </div>
-                  <AddPaymentDialog importer={importer}/>
+                  <AddPaymentDialog type="importer" id={importer}/>
                 </CardSection>
             </div>
           }
@@ -192,45 +183,4 @@ export default function ImporterTransactionPage() {
 			</div>
 		</Layout>
 	)
-}
-
-function TransactionRow({ type, id, transactionType, transactionId, date, title, details, value}: { 
-  type: string, id: string, transactionType: string, transactionId: string, date: string; title: string; details?: string; value: number;}) {
-  return (
-    <div className={cn(
-                "group flex items-center justify-between",
-                "p-2 rounded-lg",
-                "hover:bg-secondary",
-                "transition-all duration-200",
-              )}>
-      <div className="w-full flex items-center gap-2 lg:gap-6">
-        <div className="font-mono text-xs lg:text-[15px]">{date}</div>
-        <div className="grow">
-          <div className="font-medium text-zinc-900 dark:text-zinc-100">{title}</div>
-          {details && (
-            <p className="text-sm text-muted-foreground">{details}</p>
-          )}
-        </div>
-        <span className="text-base lg:text-xl font-medium">{formatCurrency(value, 2)}</span>
-        <DeleteTransactionDialog
-          type={type}
-          id={id}
-          transactionType = {transactionType}
-          transactionId={transactionId}
-        >
-          <div className={cn(
-                "group flex items-center justify-between",
-                "bg-secondary p-2 my-4 rounded-lg",
-                "transition-all duration-200",
-              )}>
-            <div className="w-full flex items-center gap-2 lg:gap-6">
-              <div className="font-mono text-xs lg:text-sm">{date}</div>
-              <div className="grow font-medium">{title}</div>
-              <span className="text-base lg:text-xl font-medium">{formatCurrency(value, 2)}</span>
-            </div>
-          </div>
-        </DeleteTransactionDialog>
-      </div>
-    </div>
-  )
 }

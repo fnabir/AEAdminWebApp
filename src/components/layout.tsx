3d594816e085f7d4926177d97c0ead74f088e1d3
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-06-19 19:13:48 +0100

    Simplify footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -44,33 +44,22 @@ const Layout: FC<{ breadcrumb: BreadcrumbInterface[], children: ReactNode }> = (
           <div className="flex-1 px-2 md:px-4 overflow-y-auto">
             {children}
           </div>
-          <footer
-            className={`hidden md:flex text-center shrink-0 items-center md:justify-between gap-2 mx-4 mt-2 transition-[width,height] ease-linear p-4 rounded-xl bg-muted/100 text-sm text-primary`}>
-            <span className="flex-1 text-center md:text-left">
+          <footer className="hidden md:flex shrink-0 items-center md:justify-between space-x-4 mx-4 my-2 p-4 rounded-xl bg-muted text-sm">
+            <span>
               © {new Date().getFullYear()} Ahsan Enterprise. All Rights Reserved.
             </span>
-              <ul className="flex space-x-4">
-                <li>
-                  <a
-                    className="hover:underline"
-                    href="https://asianliftbd.com/privacy-policy"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Privacy Policy
-                  </a>
-                </li>
-                <li>
-                  <a
-                    className="hover:underline"
-                    href="https://asianliftbd.com/terms-of-use"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    Terms Of Use
-                  </a>
-                </li>
-              </ul>
+            <ul className="flex space-x-4">
+              <li>
+                <a href="https://asianliftbd.com/privacy-policy" target="_blank" className="hover:underline" rel="noopener noreferrer">
+                  Privacy Policy
+                </a>
+              </li>
+              <li>
+                <a href="https://asianliftbd.com/terms-of-use" target="_blank" className="hover:underline" rel="noopener noreferrer">
+                  Terms Of Use
+                </a>
+              </li>
+            </ul>
           </footer>
         </div>
       </SidebarInset>

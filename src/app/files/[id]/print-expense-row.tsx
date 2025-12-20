1b3da0bb8eb994b8186a4a621c56fbde67feac0b
Farhan Noor Abir <fnoor.abir@gmail.com>
2025-12-20 14:22:19 +0000

    Fix balance alignment with details expanding over single line

--- src/app/files/[id]/print-expense-row.tsx
+++ src/app/files/[id]/print-expense-row.tsx
@@ -10,27 +10,48 @@ const PrintExpenseRow = ({ data, total }: PrintExpenseRowProps) => {
   return (
     data &&
     data.length > 0 && (
-      <tr className="w-full border-4 border-double border-accent-foreground">
-        <td className={'w-2/3 py-2 pl-6 pr-2'}>
-          {data.map((item, index) => (
-            <div key={index}>{item.val().details}</div>
-          ))}
-        </td>
-        <td className={'w-1/6 px-1.5 py-2 border-x border-accent-foreground'}>
-          {data.map((item, index) => (
-            <div key={index} className="flex">
-              <div className="flex-1">TK.</div>
-              {formatCurrency(item.val().value, 2, '')}
-            </div>
-          ))}
-        </td>
-        <td className={'w-1/6 px-1.5 py-2'}>
-          <div className="flex">
-            <div className="flex-1">TK.</div>
-            <div>{formatCurrency(total, 2, '')}</div>
-          </div>
-        </td>
-      </tr>
+      <>
+        {data.map((item, index) => (
+          <tr
+            key={index}
+            className="border-l-4 border-r-4 border-accent-foreground"
+          >
+            <td
+              className={`w-2/3 pl-6 pr-2 align-top ${
+                index === 0 ? 'pt-2' : ''
+              } ${index === data.length - 1 ? 'pb-2' : ''}`}
+            >
+              {item.val().details}
+            </td>
+
+            <td
+              className={`w-1/6 px-1.5 border-x border-accent-foreground ${
+                data.length == 1 ? '' : 'align-top'
+              } ${index === 0 ? 'pt-2' : ''} ${
+                index === data.length - 1 ? 'pb-2' : ''
+              }`}
+            >
+              <div className="flex">
+                <div className="flex-1">TK.</div>
+                {formatCurrency(item.val().value, 2, '')}
+              </div>
+            </td>
+
+            {index === 0 && (
+              <td className="w-1/6 px-1.5 py-2" rowSpan={data.length}>
+                <div className="flex">
+                  <div className="flex-1">TK.</div>
+                  <div>{formatCurrency(total, 2, '')}</div>
+                </div>
+              </td>
+            )}
+          </tr>
+        ))}
+
+        <tr className="border-b-4 border-double border-accent-foreground">
+          <td colSpan={3} />
+        </tr>
+      </>
     )
   );
 };

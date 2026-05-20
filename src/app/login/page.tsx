80e986a1b905a7bb77ae87f9b1f0e73d96eabca0
Farhan Noor Abir <fnoor.abir@gmail.com>
2026-05-20 10:23:09 +0100

    Update dependencies

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,27 +1,27 @@
-'use client';
+"use client";
 
-import { useRouter } from 'next/navigation';
-import { useForm } from 'react-hook-form';
-import { LoginFormSchema, LoginFormData } from '@/lib/schemas';
-import { zodResolver } from '@hookform/resolvers/zod';
-import { auth } from '@/firebase/config';
-import { getDatabaseReference, showToast } from '@/lib/utils';
-import Image from 'next/image';
-import TextLogo from '@/images/logo.svg';
+import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { LoginFormSchema, LoginFormData } from "@/lib/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { auth } from "@/firebase/config";
+import { getDatabaseReference, showToast } from "@/lib/utils";
+import Image from "next/image";
+import TextLogo from "@/images/logo.svg";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from '@/components/ui/card';
-import InputText from '@/components/generic/input-text';
-import ResetPassword from '@/app/login/resetPassword';
-import { login, logout } from '@/lib/functions';
-import { FirebaseError } from 'firebase/app';
-import InputPassword from '@/components/generic/input-password';
-import { get } from 'firebase/database';
-import { ButtonLoading } from '@/components/generic/button-loading';
+} from "@/components/ui/card";
+import InputText from "@/components/generic/input-text";
+import ResetPassword from "@/app/login/resetPassword";
+import { login, logout } from "@/lib/functions";
+import { FirebaseError } from "firebase/app";
+import InputPassword from "@/components/generic/input-password";
+import { get } from "firebase/database";
+import { ButtonLoading } from "@/components/generic/button-loading";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -37,12 +37,12 @@ export default function LoginPage() {
   const onSubmit = async (data: LoginFormData) => {
     try {
       await login(data.email, data.password);
-      console.log('Logged in successfully');
+      console.log("Logged in successfully");
 
       const user = auth.currentUser;
 
       if (!user) {
-        showToast('Error', 'No user found after login.', 'error');
+        showToast("Error", "No user found after login.", "error");
         return;
       }
 
@@ -51,33 +51,33 @@ export default function LoginPage() {
       );
 
       if (snapshot?.val() === true) {
-        router.push('/');
+        router.push("/");
       } else {
         await logout();
         showToast(
-          'Access Denied',
-          'Not authorized to use this service',
-          'error',
+          "Access Denied",
+          "Not authorized to use this service",
+          "error",
         );
       }
     } catch (e) {
       const error = e as FirebaseError;
       console.log(error);
       switch (error.code) {
-        case 'auth/invalid-email':
-          showToast('Error', 'Invalid email address!', 'error');
+        case "auth/invalid-email":
+          showToast("Error", "Invalid email address!", "error");
           break;
-        case 'auth/user-not-found':
-          showToast('Error', 'Email not registered with us!', 'error');
+        case "auth/user-not-found":
+          showToast("Error", "Email not registered with us!", "error");
           break;
-        case 'auth/wrong-password':
-          showToast('Error', 'Wrong password!', 'error');
+        case "auth/wrong-password":
+          showToast("Error", "Wrong password!", "error");
           break;
-        case 'auth/network-request-failed':
-          showToast('Error', 'Network connection issue!', 'error');
+        case "auth/network-request-failed":
+          showToast("Error", "Network connection issue!", "error");
           break;
         default:
-          showToast('Error', 'Invalid email/password!', 'error');
+          showToast("Error", "Invalid email/password!", "error");
           break;
       }
     }
@@ -92,8 +92,8 @@ export default function LoginPage() {
             <Image
               className={`size-24 pt-10 mx-auto`}
               src={TextLogo}
-              alt={'Ahsan Enterprise'}
-              priority={false}
+              alt={"Ahsan Enterprise"}
+              priority={true}
             />
             <div className="text-2xl font-bold">AHSAN ENTERPRISE</div>
             <CardTitle className="text-2xl">Login</CardTitle>
@@ -104,20 +104,20 @@ export default function LoginPage() {
           <CardContent>
             <form onSubmit={handleSubmit(onSubmit)} className="grid gap-6">
               <InputText
-                type={'email'}
-                label={'Email'}
+                type={"email"}
+                label={"Email"}
                 floating={false}
-                {...register('email')}
+                {...register("email")}
                 placeholder="user@ahsanenterprise.com"
-                error={errors.email?.message || ''}
+                error={errors.email?.message || ""}
                 required
               />
               <InputPassword
-                label={'Password'}
+                label={"Password"}
                 floating={false}
-                {...register('password')}
+                {...register("password")}
                 placeholder="******"
-                error={errors.password?.message || ''}
+                error={errors.password?.message || ""}
                 required
               />
               <ButtonLoading
@@ -140,8 +140,8 @@ export default function LoginPage() {
                 rel="noopener noreferrer"
               >
                 Terms of Use
-              </a>{' '}
-              and{' '}
+              </a>{" "}
+              and{" "}
               <a
                 className="underline"
                 target="_blank"
